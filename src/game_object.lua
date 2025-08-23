3e98906d948694745770808da4fce56bfd86ca72
treeman0013 <connorwillchris@gmail.com>
2025-08-23 12:52:04 -0600

    another commit

--- src/game_object.lua
+++ src/game_object.lua
@@ -3,21 +3,24 @@
 ]]
 
 GAME.GameObject = {
-    x = 0, y = 0, z = 0,
+    x = 0,
+    y = 0,
+    z = 0,
 
     new = function(self, x, y, z)
-        local obj = {}
-        setmetatable(obj, self)
-        self.__index = self
-        obj.x = x
-        obj.y = y
-        obj.z = z
-        return obj
+        local obj = {};
+        setmetatable(obj, self);
+        self.__index = self;
+        obj.x = x;
+        obj.y = y;
+        obj.z = z;
+        return obj;
     end,
 
-    --[[asdf = function(self)
-
+    --[[print = function(self)
+        print("GameObject at (" .. self.x .. ", " .. self.y .. ", " .. self.z .. ")");
     end]]
 };
 
 --local a = GAME.GameObject.new(0, 1, 2);
+local a = GAME.GameObject.new(0, 1, 2);
